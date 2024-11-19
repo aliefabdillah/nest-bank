f09cb696d44c93be1c8de85e6ee80820ef14efdf
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-19 18:56:39 +0700

    fix: disable auto create timestamps

--- src/module/transactions/entities/transactionsAccounts.entity.ts
+++ src/module/transactions/entities/transactionsAccounts.entity.ts
@@ -2,7 +2,10 @@ import { Column, ForeignKey, Model, Table } from 'sequelize-typescript';
 import { Accounts } from 'src/module/accounts/entities/accounts.entity';
 import { Transactions } from './transactions.entity';
 
-@Table
+@Table({
+  updatedAt: false,
+  createdAt: false,
+})
 export class TransactionAccounts extends Model {
   @ForeignKey(() => Accounts)
   @Column
