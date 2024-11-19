ac9a86fab3f39c9c41c571ffe4cfe4f9695f1f2a
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-19 18:50:12 +0700

    fix: add new import sequelize schema into module

--- src/module/transactions/transactions.module.ts
+++ src/module/transactions/transactions.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Transactions } from './entities/transactions.entity';
 
-@Module({})
+@Module({
+  imports: [SequelizeModule.forFeature([Transactions])],
+})
 export class TransactionsModule {}
