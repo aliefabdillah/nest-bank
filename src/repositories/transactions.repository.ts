d58320000f9e503a01eeb5f8a610ac47af400fdb
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-26 00:15:14 +0700

    feat: create transactuibs repository class

---
+++ src/repositories/transactions.repository.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { BaseAbstractRepository } from './base.abstract.repository';
+import { BaseInterfaceRepository } from './base.interface.repository';
+import { InjectModel } from '@nestjs/sequelize';
+import { Transactions } from 'src/module/transactions/entities/transactions.entity';
+
+@Injectable()
+export class TransactionsRepository
+  extends BaseAbstractRepository<Transactions>
+  implements BaseInterfaceRepository<Transactions>
+{
+  constructor(
+    @InjectModel(Transactions)
+    private readonly transactionsRepository: typeof Transactions,
+  ) {
+    super(transactionsRepository);
+  }
+}
