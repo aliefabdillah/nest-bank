781d9da076a63b4bb2444ef33cc96f9699673d2d
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-25 23:58:47 +0700

    feat: create interface class for base repository

---
+++ src/repositories/base.interface.repository.ts
@@ -0,0 +1,8 @@
+export interface BaseInterfaceRepository<T> {
+  create(data: T | any): Promise<T>;
+  findAll(): Promise<T[]>;
+  findById(id: string): Promise<T>;
+  update(id: string, data: T | any): Promise<[number, T[]]>;
+  delete(id: string): Promise<number>; //return of number row deleted
+  findWithRelation(relation: any): Promise<T[]>;
+}
