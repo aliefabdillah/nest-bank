d42973a315f09ec7d695ccd1653b2bd3c9e733c3
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-26 20:30:52 +0700

    feat: create login endpoint

--- src/module/users/users.controller.ts
+++ src/module/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { UsersServiceInterface } from './interface/user.service.interface';
 import { RegisterUserDto } from './dto/registerUser.dto';
+import { LoginUserDto } from './dto/loginUser.dto';
 
 @Controller('users')
 export class UsersController {
@@ -12,5 +13,11 @@ export class UsersController {
   @Post('register')
   async register(@Body() registerDto: RegisterUserDto) {
     return this.usersService.register(registerDto);
+  }
+
+  @Post('login')
+  async login(@Body() loginDto: LoginUserDto) {
+    console.log(process.env.JWT_SECRET);
+    return this.usersService.login(loginDto);
   }
 }
