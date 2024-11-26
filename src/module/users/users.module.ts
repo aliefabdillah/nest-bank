e9fc1cf3c79bd404936e176d4997e5d38ade978e
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-26 19:46:48 +0700

    chore: implement providers for users repository and user service

--- src/module/users/users.module.ts
+++ src/module/users/users.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Users } from './entities/users.entity';
+import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { UsersRepository } from 'src/repositories/users.repository';
 
 @Module({
   imports: [SequelizeModule.forFeature([Users])],
   exports: [SequelizeModule],
+  providers: [
+    UsersRepository,
+    {
+      provide: 'UsersServiceInterface',
+      useClass: UsersService,
+    },
+  ],
+  controllers: [UsersController],
 })
 export class UsersModule {}
