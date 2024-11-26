e138d0bbe18929c313a858854fcbb7e52b12c510
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-26 20:10:49 +0700

    feat: import require jwt and passport module

--- src/module/users/users.module.ts
+++ src/module/users/users.module.ts
@@ -4,9 +4,19 @@ import { Users } from './entities/users.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UsersRepository } from 'src/repositories/users.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Users])],
+  imports: [
+    SequelizeModule.forFeature([Users]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
   exports: [SequelizeModule],
   providers: [
     UsersRepository,
@@ -14,6 +24,7 @@ import { UsersRepository } from 'src/repositories/users.repository';
       provide: 'UsersServiceInterface',
       useClass: UsersService,
     },
+    JwtStrategy,
   ],
   controllers: [UsersController],
 })
