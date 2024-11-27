e02e8d49060ca159e218faf83a3ee60a6ea537af
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-27 08:34:26 +0700

    feat: import module for accounts service

--- src/module/accounts/accounts.module.ts
+++ src/module/accounts/accounts.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Accounts } from './entities/accounts.entity';
+import { AccountsService } from './accounts.service';
+import { AccountsRepository } from 'src/repositories/accounts.repository';
 
 @Module({
   imports: [SequelizeModule.forFeature([Accounts])],
+  providers: [
+    AccountsRepository,
+    { provide: 'AccountsServiceInterface', useClass: AccountsService },
+  ],
 })
 export class AccountsModule {}
