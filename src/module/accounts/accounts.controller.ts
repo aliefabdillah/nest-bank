4122a3184c1f3d60c85c0ad6e0164d80a2960e2c
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-27 09:22:52 +0700

    feat: create account controller

---
+++ src/module/accounts/accounts.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Inject, Post, Req, UseGuards } from '@nestjs/common';
+import { AccountsServiceInterface } from './interface/accounts.service.interface';
+import { CreateAccountDto } from './dto/create-account.dto';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('accounts')
+export class AccountsController {
+  constructor(
+    @Inject('AccountsServiceInterface')
+    private readonly accountsService: AccountsServiceInterface,
+  ) {}
+
+  @UseGuards(AuthGuard())
+  @Post()
+  async createAccounts(@Req() req: any, @Body() accountDto: CreateAccountDto) {
+    return this.accountsService.create(accountDto, req.user.id);
+  }
+}
--- src/module/accounts/accounts.module.ts
+++ src/module/accounts/accounts.module.ts
@@ -3,12 +3,15 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Accounts } from './entities/accounts.entity';
 import { AccountsService } from './accounts.service';
 import { AccountsRepository } from 'src/repositories/accounts.repository';
+import { AccountsController } from './accounts.controller';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [SequelizeModule.forFeature([Accounts])],
+  imports: [SequelizeModule.forFeature([Accounts]), UsersModule],
   providers: [
     AccountsRepository,
     { provide: 'AccountsServiceInterface', useClass: AccountsService },
   ],
+  controllers: [AccountsController],
 })
 export class AccountsModule {}
