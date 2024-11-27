fadfa21334477950990c7bb527e3d03368b275dc
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-27 08:30:30 +0700

    feat: create account service interface

--- src/module/accounts/interface/accounts.service.interface.ts
+++ src/module/accounts/interface/accounts.service.interface.ts
@@ -5,5 +5,5 @@ import { Response } from 'src/Response/response';
 export interface AccountsServiceInterface {
   create(accountDto: CreateAccountDto): Promise<Response<Accounts>>;
   getAccounts(): Promise<Response<Accounts>>;
-  getDetailAccount(id: string): Promise<Response<Accounts>>;
+  getAccountById(id: string): Promise<Response<Accounts>>;
 }
