fbbd52418010ad46de2bb8cd2d72b9a58b55ea75
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-27 09:21:01 +0700

    feat: create logic service create

--- src/module/accounts/accounts.service.ts
+++ src/module/accounts/accounts.service.ts
@@ -1,17 +1,37 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { AccountsRepository } from 'src/repositories/accounts.repository';
 import { AccountsServiceInterface } from './interface/accounts.service.interface';
 import { Response } from 'src/Response/response';
 import { CreateAccountDto } from './dto/create-account.dto';
 import { Accounts } from './entities/accounts.entity';
+import { randomInt } from 'crypto';
 
 @Injectable()
 export class AccountsService implements AccountsServiceInterface {
   constructor(private readonly accountsRepository: AccountsRepository) {}
 
-  create(accountDto: CreateAccountDto): Promise<Response<Accounts>> {
-    throw new Error('Method not implemented.');
+  async create(
+    accountDto: CreateAccountDto,
+    id: string,
+  ): Promise<Response<Accounts>> {
+    const account_number =
+      '6078' +
+      (new Date().getMonth() + 1) +
+      randomInt(1000000000, 10000000000).toString();
+
+    console.log(accountDto.account_type);
+    const accountsData = await this.accountsRepository.create({
+      account_number: account_number,
+      account_type: accountDto.account_type,
+      user_id: id,
+    });
+
+    return {
+      status: HttpStatus.CREATED,
+      data: accountsData,
+      message: 'Created Accounts succesfully',
+    };
   }
 
   getAccounts(): Promise<Response<Accounts>> {
