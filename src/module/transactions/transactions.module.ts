3d987f3e2fec9ae453a00038b6b2bd143243c5d3
alief abdillah <aliefmabdillah09@gmail.com>
2024-11-27 17:01:58 +0700

    feat: setup providers for transactions module

--- src/module/transactions/transactions.module.ts
+++ src/module/transactions/transactions.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Transactions } from './entities/transactions.entity';
+import { TransactionsService } from './transactions.service';
+import { TransactionsRepository } from 'src/repositories/transactions.repository';
 
 @Module({
   imports: [SequelizeModule.forFeature([Transactions])],
+  providers: [
+    TransactionsRepository,
+    { provide: 'TransactionServiceInterface', useClass: TransactionsService },
+  ],
 })
 export class TransactionsModule {}
